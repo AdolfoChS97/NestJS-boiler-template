143d1d87ceb5058f27546a2731cdcbd3ced118a8
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-01-02 17:13:56 -0400

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,30 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API boilerplate')
+    .setDescription('A NestJS API boilerplate')
+    .setVersion('1.0')
+    .build();
+
   const configService = app.get(ConfigService);
   const APP_ORIGIN = configService.get('APP_ORIGIN');
   app.enableCors({ origin: APP_ORIGIN });
   const PORT = configService.get('APP_PORT');
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  Logger.log(
+    `Swagger docs running on http://localhost:${PORT}/docs`,
+    'Bootstrap',
+  );
+
   await app.listen(PORT, () => {
     Logger.log(`Listening on port ${PORT}`, 'Bootstrap');
   });
