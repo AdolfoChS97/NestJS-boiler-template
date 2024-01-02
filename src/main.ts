29043524a0aa128947fa26919f5153da7ba13f4f
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-01-02 17:10:07 -0400

    chore: logging port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
   const APP_ORIGIN = configService.get('APP_ORIGIN');
   app.enableCors({ origin: APP_ORIGIN });
   const PORT = configService.get('APP_PORT');
-  await app.listen(PORT);
+  await app.listen(PORT, () => {
+    Logger.log(`Listening on port ${PORT}`, 'Bootstrap');
+  });
 }
 bootstrap();
