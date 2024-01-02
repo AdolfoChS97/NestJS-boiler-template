1b5f3c67c7019173c2198c606ecbcc93c3d3cf35
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-01-02 17:05:08 -0400

    feat: add helmet

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(helmet());
   const configService = app.get(ConfigService);
   const APP_ORIGIN = configService.get('APP_ORIGIN');
   app.enableCors({ origin: APP_ORIGIN });
